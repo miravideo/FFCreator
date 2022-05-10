342860072139905e16b03721b7c26c0325660b20
penghui <penghui@gmail.com>
2022-05-10 19:15:27 +0800

    enhance: prepareMaterial video时比较耗时，这里把prepareMaterial的进度状态一起打印出来，方便后续加上进度查询。

--- burner.js
+++ burner.js
@@ -20,16 +20,24 @@ const burn = async (opts) => {
     console.log(pp);
   });
 
+  let t = Date.now();
   creator.on('start', () => {
-    console.log(`Burn start`);
+    console.log(`Burn start.`);
+    console.log(`Burn start timestamp: ${Date.now() - t}ms`);
   }).on('error', e => {
     console.error(e);
   }).on('progress', e => {
     console.log(`Burn progress: ${(e.percent * 100) >> 0}%`);
+    console.log(`Burn progress timestamp: ${Date.now() - t}ms`);
   }).on('preloading', (evt) => {
-    console.log(`Burn preloading ${evt.id}: ${evt.loaded}/${evt.total}`);
+    console.log(`Burn preloading: ${evt.id}: ${evt.loaded}/${evt.total}`);
+    console.log(`Burn preloading timestamp: ${Date.now() - t}ms`);
+  }).on('prepareMaterial', (evt) => {
+    console.log(`Burn prepareMaterial: ${evt.id}: ${evt.prepared}/${evt.total}`);
+    console.log(`Burn prepareMaterial timestamp: ${Date.now() - t}ms`);
   }).on('complete', e => {
     console.log(`Burn completed: \n USEAGE: ${e.useage} \n PATH: ${e.output} `);
+    console.log(`Burn completed timestamp: ${Date.now() - t}ms`);
   }).generateOutput().start();
 }
 
