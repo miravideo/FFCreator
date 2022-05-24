0289052127148c4a0773a132bab2525c54063cac
Zhao Jun <leo.zhaojun@gmail.com>
2022-05-24 18:09:09 +0800

    video speed & text style update

--- test/unit/time/video.test.js
+++ test/unit/time/video.test.js
@@ -71,3 +71,82 @@ describe('time/video', () => {
     expect(clip.endTime).toBe(10);
   });
 });
+
+const VideoMaterial = require('@/material/video');
+describe('time/video-material', () => {
+  test('video material ss/to', () => {
+    const mat = new VideoMaterial({ ss: 3, to: 5 });
+    mat.duration = 3;
+    expect(mat.getStartOffset()).toBe(3);
+    expect(mat.getEndOffset()).toBe(5);
+    expect(mat.getEndOffset(true)).toBe(5);
+    expect(mat.getDuration()).toBe(2);
+
+    mat.length = 4.5;
+    expect(mat.getEndOffset()).toBe(4.5);
+    expect(mat.getDuration()).toBe(1.5);
+
+    // 参数 withConainer = true 会计算外部容器duration的影响
+    mat.duration = 1;
+    expect(mat.getEndOffset()).toBe(4.5);
+    expect(mat.getEndOffset(true)).toBe(4);
+    expect(mat.getDuration()).toBe(1.5);
+    expect(mat.getDuration(true)).toBe(1);
+  });
+
+  test('video material speed', () => {
+    const mat = new VideoMaterial({ ss: 3, to: 9, speed: 3 });
+    mat.duration = 3;
+    mat.length = 10;
+    expect(mat.getStartOffset()).toBe(3);
+    expect(mat.getEndOffset()).toBe(9);
+    expect(mat.getEndOffset(true)).toBe(9);
+    expect(mat.getDuration()).toBe(2);
+    expect(mat.getDuration(true)).toBe(2);
+
+    mat.speed = 2;
+    mat.duration = 2.5;
+    expect(mat.getEndOffset()).toBe(9);
+    expect(mat.getEndOffset(true)).toBe(8); // 3 + (2.5 * 2) = 8
+    expect(mat.getDuration()).toBe(3);
+    expect(mat.getDuration(true)).toBe(2.5);
+    expect(mat.seekTime(1)).toBe(5);
+    expect(mat.seekTime(5)).toBe(9);
+    expect(mat.seekTime(10)).toBe(9);
+  });
+
+  test('video material default value', () => {
+    const mat = new VideoMaterial({ speed: 2 });
+    mat.duration = 3;
+    mat.length = 12;
+    expect(mat.getStartOffset()).toBe(0);
+    expect(mat.getEndOffset()).toBe(12);
+    expect(mat.getEndOffset(true)).toBe(6);
+    expect(mat.getDuration()).toBe(6);
+    expect(mat.getDuration(true)).toBe(3);
+    expect(mat.seekTime(1)).toBe(2);
+    expect(mat.seekTime(5)).toBe(10);
+    expect(mat.seekTime(10)).toBe(12);
+  });
+
+  test('video material with loop', () => {
+    const video = new FFVideo({ loop: true });
+    video.parent = { absStartTime: 0 };
+    const mat = new VideoMaterial({ ss: 3, to: 9, speed: 2, loop: true });
+    video.material = mat;
+    mat.duration = 10;
+    mat.length = 12;
+    expect(mat.getStartOffset()).toBe(3);
+    expect(mat.getEndOffset()).toBe(9);
+    expect(mat.getEndOffset(true)).toBe(9);
+    expect(mat.getDuration()).toBe(3);
+    expect(mat.getDuration(true)).toBe(3);
+    expect(mat.seekTime(1)).toBe(5); // 3 + (1 * 2)
+    expect(mat.seekTime(5)).toBe(9); // max 9
+    expect(video.materialTime(5).time).toBe(2); // 5 % 3 = 2
+    expect(video.materialTime(5, true).time).toBe(7); // 3 + (2 * (5 % 3))
+    expect(video.materialTime(10).time).toBe(1); // 10 % 3 = 1
+    expect(video.materialTime(10, true).time).toBe(5); // 3 + (2 * (10 % 3))
+    expect(video.materialTime(10, true).loops).toBe(3); // floor(10 / 3)
+  });
+});
