be27a1e30103e3d56e313379d885e711748457ee
Zhao Jun <leo.zhaojun@gmail.com>
2022-08-01 18:21:04 +0800

    get/set param

--- test/unit/core/base.test.js
+++ test/unit/core/base.test.js
@@ -30,6 +30,32 @@ describe('core/base', () => {
     expect(conf).toMatchObject({});
   });
 
+  test('get/set params', () => {
+    expect(base.getParam('key')).toBe(undefined);
+    base.setParam('key', 'value1');
+    expect(base.getParam('key')).toBe('value1');
+
+    expect(base.getParam('key.sub')).toBe(undefined);
+    base.setParam('key.sub', 'value2');
+    expect(base.getParam('key.sub')).toBe('value2');
+    expect(base.getParam('key')).toMatchObject({'sub':'value2'});
+
+    expect(base.getParam('key.sub.sub2.sub3')).toBe(undefined);
+    base.setParam('key.sub.sub2.sub3', 'value3');
+    expect(base.getParam('key.sub.sub2.sub3')).toBe('value3');
+    expect(base.getParam('key.sub')).toMatchObject({'sub2':{'sub3':'value3'}});
+
+    base.setParam('key.arr.0', 'av1');
+    base.setParam('key.arr.1', 'av2');
+    expect(base.getParam('key.arr.0')).toBe('av1');
+    expect(base.getParam('key.arr')).toMatchObject(['av1', 'av2']);
+
+    base.setParam('key.rev.1', 'av2');
+    base.setParam('key.rev.0', 'av1');
+    expect(base.getParam('key.rev.0')).toBe('av1');
+    expect(base.getParam('key.rev')).toMatchObject(['av1', 'av2']);
+  });
+
   test('destroy: destroy function invoke success', () => {
     base.destroy();
     expect(base.parent).toBeFalsy();
