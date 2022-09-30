ee667f7dcae534813328f15f00b4cde23a52db58
a6037220 <a6037220@163.com>
2022-09-30 16:10:22 +0800

    feat: 增加音频可视化对conf变化的响应，并自适应canvas大小

--- mixin/src/timeline.js
+++ mixin/src/timeline.js
@@ -8,18 +8,34 @@ class Timeline extends Mixin {
     return { width: this.width, height: this.height, duration: this.MAX_TIME };
   }
 
+  initConf(conf) {
+    this.conf.color = conf.color || '#f2edf7';
+    this.conf.baseColor =  conf.baseColor || '#4a454d';
+  }
+
   async update(conf) {
+    if (conf.width) this.containerWidth = conf.width;
+    if (conf.height) this.containerHeight = conf.height;
+    if ((conf.width && conf.width !== this.width) || (conf.height && conf.height !== this.height)) {
+      this.resize(conf.width || this.containerWidth, conf.height || this.containerHeight);
+    }
+    Object.assign(this.conf, conf);
     this.process = conf.process;
+    return {width: this.containerWidth, height: this.containerHeight}
   }
 
   render(time, delta) {
     const canvas = this.canvas;
     const ctx = canvas.getContext('2d');
+    const width = this.containerWidth;
+    const height = this.containerHeight;
+    const {color, baseColor} = this.conf;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = '#f2edf7';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = '#4a454d';
-    ctx.fillRect(0, 0, Math.floor(canvas.width * this.process), canvas.height);
+    ctx.fillStyle = baseColor;
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, Math.floor(width * this.process), height);
   }
 }
 
